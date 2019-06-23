1704239563159c06738c2c2a1115ee7bfc148d0d
RONTheCookie <me@ronthecookie.me>
2019-06-23 11:58:36 +0300

    organize server.ts imports

--- src/Server.ts
+++ src/Server.ts
@@ -1,15 +1,12 @@
 import EventEmitter from "events";
-import mc, {
-    Server as MinecraftProtocolServer,
-    ServerOptions as MinecraftProtocolServerOptions
-} from "minecraft-protocol";
+import mc, { Server as MinecraftProtocolServer, ServerOptions as MinecraftProtocolServerOptions } from "minecraft-protocol";
+import pkg from "../package.json";
 import Command from "./Command";
 import { Logger } from "./Logger";
 import Player, { ChatMessagePart } from "./Player";
 import Plugin from "./Plugin";
 import getPlugins from "./plugins";
 import { chatMessagePartsToString } from "./Util";
-import pkg from "../package.json";
 interface ServerOptions extends MinecraftProtocolServerOptions {
     // plugins: Plugin[],
     encryption?: boolean;
@@ -34,8 +31,9 @@ class Server extends EventEmitter {
         this.server = mc.createServer(options);
         // Might want to refactor this to have a difference between internal and external plugins.
         this.plugins = getPlugins(this);
-        this.logger.info(`Server listening on ${options.host}:${options.port}`)
+        this.logger.info(`Server listening on ${options.host}:${options.port}`);
     }
+
     broadcast(text: string | ChatMessagePart[]) {
         this.players.forEach(player => player.chat(text));
         if (typeof text == "string") this.logger.info("Chat:", text);
